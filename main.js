cd1a359d67e7f889c47fa472c3b22ab300704da3
brianvecino <brian.vecino@outlook.com>
2026-01-18 20:04:52 -0300

    uso de aos y gsap

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+AOS.init();
+
+const header = document.querySelector(".hero");
+  header.addEventListener("mouseenter", () => {
+    gsap.to(header, { scale: 1.1, duration: .3, ease: "power2.out" });
+  });
+
+  header.addEventListener("mouseleave", () => {
+    gsap.to(header, { scale: 1, duration: .3, ease: "power2.out" });
+  });
+
+const cardMovie = document.querySelector("#card-movie");
+
+ cardMovie.addEventListener("mouseenter", () => {
+    gsap.to(cardMovie, { scale: 1.05, duration: .3, ease: "power2.out" });
+  });
+
+  cardMovie.addEventListener("mouseleave", () => {
+    gsap.to(cardMovie, { scale: 0.95, duration: .3, ease: "power2.out" });
+  });
+
+const cardList = document.querySelector("#card-list");
+
+ cardList.addEventListener("mouseenter", () => {
+    gsap.to(cardList, { scale: 1.05, duration: .3, ease: "power2.out" });
+  });
+
+ cardList.addEventListener("mouseleave", () => {
+    gsap.to(cardList, { scale: 0.95, duration: .3, ease: "power2.out" });
+  });
+
 document.addEventListener('DOMContentLoaded', () => {
     const copyButton = document.getElementById('copyEmailBtn');
 
